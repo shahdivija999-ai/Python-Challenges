f84228f47098acefedafacf2f88d8346c4519c68
shahdivija999-ai <shahdivija999@gmail.com>
2026-06-10 16:55:45 -0500

    Add files via upload

---
+++ Activity2.py
@@ -0,0 +1,9 @@
+print("Enter a Number (Numerator): ")
+numerator = int(input())
+print("Enter a Number (Denominator): ")
+denominator = int(input())
+
+if numerator%denominator==0:
+    print("The result is divisibe")
+else: 
+    print("The result is not divisible")
