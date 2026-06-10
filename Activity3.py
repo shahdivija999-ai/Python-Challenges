9ddc7951bd49063948bf43736a4be6d8c9fd0f96
shahdivija999-ai <shahdivija999@gmail.com>
2026-06-10 17:04:03 -0500

    Add files via upload

---
+++ Activity3.py
@@ -0,0 +1,14 @@
+mean1 = 38
+wrong_number = 36
+correct_number = 56
+total_number = 40
+
+sum = mean1*total_number
+print("The sum of 40 numbers is: ",sum)
+
+num2=sum-((wrong_number)-(correct_number))
+print("The new sum is: ",num2)
+
+
+mean2=num2/total_number
+print(mean2)
